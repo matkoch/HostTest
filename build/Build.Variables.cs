40cb938d9b01a3508583a32fb7627acd781f91fc
Matthias Koch <ithrowexceptions@gmail.com>
2021-11-01 01:24:27 +0100

    asdf

--- build/Build.Variables.cs
+++ build/Build.Variables.cs
@@ -1,6 +1,9 @@
 using System;
+using System.IO;
 using System.Linq;
 using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.IO;
 using Nuke.Common.Utilities.Collections;
 
 partial class Build
@@ -9,5 +12,17 @@ partial class Build
         .Executes(() =>
         {
             EnvironmentInfo.Variables.OrderBy(x => x.Key).ForEach(x => Console.WriteLine($"{x.Key} = {x.Value}"));
+
+            if (Host is GitHubActions actions)
+            {
+                var actionsGitHubEventPath = (AbsolutePath)actions.GitHubEventPath;
+                var readOnlyCollection = actionsGitHubEventPath.Parent.GlobFiles("**/*.*");
+                foreach (var absolutePath in readOnlyCollection)
+                {
+                    Console.WriteLine("==========");
+                    Console.WriteLine(absolutePath);
+                    Console.WriteLine(File.ReadAllText(absolutePath));
+                }
+            }
         });
 }
