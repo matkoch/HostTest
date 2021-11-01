44118a55dcb5edfd0e6677784a5d8acb82852807
Matthias Koch <ithrowexceptions@gmail.com>
2021-11-01 18:24:16 +0100

    asdf

--- build/Build.Variables.cs
+++ build/Build.Variables.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
 using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Nuke.Common;
 using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.IO;
@@ -16,13 +18,23 @@ partial class Build
             if (Host is GitHubActions actions)
             {
                 var actionsGitHubEventPath = (AbsolutePath)actions.GitHubEventPath;
-                var readOnlyCollection = actionsGitHubEventPath.Parent.GlobFiles("**/*.*");
-                foreach (var absolutePath in readOnlyCollection)
-                {
-                    Console.WriteLine("==========");
-                    Console.WriteLine(absolutePath);
-                    Console.WriteLine(File.ReadAllText(absolutePath));
-                }
+                // var readOnlyCollection = actionsGitHubEventPath.Parent.GlobFiles("**/*.*");
+                // foreach (var absolutePath in readOnlyCollection)
+                // {
+                //     Console.WriteLine("==========");
+                //     Console.WriteLine(absolutePath);
+                //     Console.WriteLine(File.ReadAllText(absolutePath));
+                // }
+
+                var content = File.ReadAllText(actions.GitHubEventPath);
+                var event2 = JsonConvert.DeserializeObject<JObject>(content);
+                Console.WriteLine(event2["number"]?.Value<string>());
+
+                var contextContent = EnvironmentInfo.GetVariable<string>("GITHUB_CONTEXT");
+                var context = JsonConvert.DeserializeObject<JObject>(contextContent);
+                Console.WriteLine(context["token"]?.Value<string>());
+                Console.WriteLine(context["run_attempt"]?.Value<string>());
             }
+
         });
 }
