edc27bb7141d6f7dc4a62721bc14f1be3363754a
Robot <ithrowexceptions+build@gmail.com>
2021-11-01 02:02:40 +0100

    Update Build.cs

--- build/Build.cs
+++ build/Build.cs
@@ -45,5 +45,5 @@ partial class Build : NukeBuild
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
-    public static int Main() => Execute<Build>(x => x.Compile);
+    public static int Main()   => Execute<Build>(x => x.Compile);
 }
